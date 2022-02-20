35f1b763a5dc5b8b04dbeb85a12e9f36cf9d57e5
Bartlomiej Plotka <bwplotka@gmail.com>
2022-02-20 11:12:14 +0100

    cgroup v2 are... a mess. You need access to /proc/<pid> and removal of cgroup is funky (don't yet know how to do it).
    Signed-off-by: Bartlomiej Plotka <bwplotka@gmail.com>

--- monitoring/cgroups_linux.go
+++ monitoring/cgroups_linux.go
@@ -39,7 +39,7 @@ func v2MountPoint() (string, error) {
 			return "", fmt.Errorf("mountinfo: bad entry %q", text)
 		}
 		if fields[numFields-3] == "cgroup2" {
-			return filepath.Dir(fields[4]), nil
+			return fields[4], nil
 		}
 	}
 	if err := scanner.Err(); err != nil {
@@ -54,7 +54,43 @@ func setupPIDAsContainer(env e2e.Environment, pid int) ([]string, error) {
 		return nil, errors.Wrap(err, "find v2 mountpoint")
 	}
 
-	mgr, err := cgroups.LoadManager(mountpoint, filepath.Join(cgroupSubGroup, env.Name()))
+	// Try to create test cgroup to check if we have permission.
+	{
+		mgr, err := cgroups.NewManager(mountpoint, "/"+filepath.Join(cgroupSubGroup, "__test__"), &cgroups.Resources{})
+		if err != nil {
+			if !os.IsPermission(err) {
+				return nil, errors.Wrap(err, "new test cgroup")
+			}
+
+			uid := os.Getuid()
+			cmds := fmt.Sprintf("sudo mkdir -p %s && sudo chown -R %d %s",
+				filepath.Join(mountpoint, cgroupSubGroup),
+				uid,
+				filepath.Join(mountpoint, cgroupSubGroup),
+			)
+			return nil, errors.Errorf("e2e does not have permissions, run following command: %q; err: %v", cmds, err)
+		}
+		if err := mgr.Delete(); err != nil {
+			return nil, errors.Wrap(err, "delete test")
+		}
+	}
+
+	// Delete previous cgroup if it exists.
+	mgr, err := cgroups.LoadManager(mountpoint, "/"+filepath.Join(cgroupSubGroup, env.Name()))
+	if err != nil {
+		// Deleted?
+		return nil, errors.Wrap(err, "load cgroup")
+	} else {
+		if err := mgr.Freeze(); err != nil {
+			return nil, errors.Wrap(err, "freeze")
+		}
+		if err := mgr.Delete(); err != nil {
+			return nil, errors.Wrap(err, "delete")
+		}
+	}
+
+	// Create cgroup that will contain our process.
+	mgr, err = cgroups.NewManager(mountpoint, "/"+filepath.Join(cgroupSubGroup, env.Name()), &cgroups.Resources{})
 	if err != nil {
 		return nil, errors.Wrap(err, "new v2 manager")
 	}
@@ -63,82 +99,18 @@ func setupPIDAsContainer(env e2e.Environment, pid int) ([]string, error) {
 		return nil, errors.Wrap(err, "add proc")
 	}
 
-	//// Try to setup test cgroup to check if we have perms.
-	//{
-	//	c, err := cgroups.New(cgroups.V1, cgroups.StaticPath(filepath.Join(cgroupSubGroup, "__test__")), &specs.LinuxResources{})
-	//	if err != nil {
-	//		if !os.IsPermission(err) {
-	//			return nil, errors.Wrap(err, "new test cgroup")
-	//		}
-	//
-	//		uid := os.Getuid()
-	//
-	//		var cmds []string
-	//
-	//		ss, cerr := cgroups.V1()
-	//		if cerr != nil {
-	//			return nil, errors.Wrap(cerr, "access v1 test")
-	//		}
-	//
-	//		for _, s := range ss {
-	//			cmds = append(cmds, fmt.Sprintf("sudo mkdir -p %s && sudo chown -R %d %s",
-	//				filepath.Join(mountpoint, string(s.Name()), cgroupSubGroup),
-	//				uid,
-	//				filepath.Join(mountpoint, string(s.Name()), cgroupSubGroup),
-	//			))
-	//		}
-	//		return nil, errors.Errorf("e2e does not have permissions, run following command: %q; err: %v", strings.Join(cmds, " && "), err)
-	//	}
-	//	if err := c.Delete(); err != nil {
-	//		return nil, errors.Wrap(err, "delete test")
-	//	}
-	//}
-	//
-	//// Delete previous cgroup if it exists.
-	//root, err := cgroups.Load(cgroups.V1, cgroups.RootPath)
-	//if err != nil {
-	//	return nil, errors.Wrap(err, "load root")
-	//}
-	//
-	//l, err := cgroups.Load(cgroups.V1, cgroups.StaticPath(filepath.Join(cgroupSubGroup, env.Name())))
-	//if err != nil {
-	//	if err != cgroups.ErrCgroupDeleted {
-	//		return nil, err
-	//	}
-	//} else {
-	//	if err := l.MoveTo(root); err != nil {
-	//		return nil, err
-	//	}
-	//	if err := l.Delete(); err != nil {
-	//		return nil, err
-	//	}
-	//}
-	//
-	//// Create cgroup that will contain our process.
-	//c, err := cgroups.New(cgroups.V1, cgroups.StaticPath(filepath.Join(cgroupSubGroup, env.Name())), &specs.LinuxResources{})
-	//if err != nil {
-	//	return nil, err
-	//}
-	//if err := c.Add(cgroups.Process{Pid: pid}); err != nil {
-	//	return nil, err
-	//}
-	//env.AddCloser(func() {
-	//	l, err := cgroups.Load(cgroups.V1, cgroups.StaticPath(filepath.Join(cgroupSubGroup, env.Name())))
-	//	if err != nil {
-	//		if err != cgroups.ErrCgroupDeleted {
-	//			// All good.
-	//			return
-	//		}
-	//		fmt.Println("Failed to load cgroup", err)
-	//	}
-	//	if err := l.MoveTo(root); err != nil {
-	//		fmt.Println("Failed to move all processes", err)
-	//	}
-	//	if err := c.Delete(); err != nil {
-	//		// TODO(bwplotka): This never works, but not very important, fix it.
-	//		fmt.Println("Failed to delete cgroup", err)
-	//	}
-	//})
+	env.AddCloser(func() {
+		mgr, err := cgroups.LoadManager(mountpoint, "/"+filepath.Join(cgroupSubGroup, env.Name()))
+		if err != nil {
+			// Deleted?
+			fmt.Println("Failed to load cgroup", err)
+			return
+		}
+		if err := mgr.Delete(); err != nil {
+			// TODO(bwplotka): This never works, but not very important, fix it.
+			fmt.Println("Failed to delete cgroup", err)
+		}
+	})
 
 	return []string{filepath.Join("/", cgroupSubGroup, env.Name())}, nil
 }
