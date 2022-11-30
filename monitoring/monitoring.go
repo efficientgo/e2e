1ff6b13a40ac46e0b7602c616e6112cf02bb0beb
Douglas Camata <159076+douglascamata@users.noreply.github.com>
2022-11-30 14:31:53 +0100

    Fix host-local endpoint test in WSL2
    Signed-off-by: Douglas Camata <159076+douglascamata@users.noreply.github.com>

--- monitoring/monitoring.go
+++ monitoring/monitoring.go
@@ -260,7 +260,16 @@ func Start(env e2e.Environment, opts ...Option) (_ *Service, err error) {
 	}))
 
 	// Listen on all addresses, since we need to connect to it from docker container.
-	list, err := net.Listen("tcp", "0.0.0.0:0")
+	networkType := "tcp"
+	// For unknown reasons, when using WSL 2, if the network type is "tcp" it will
+	// end up only binding to the IPv6 in the WSL host, which later cannot be acessed
+	// via IPv4 to confirm Prometheus can scrape the local endpoint.
+	// Explicitly asking for an IPv4 listener works.
+	wsl2 := env.WSL2()
+	if wsl2 {
+		networkType = "tcp4"
+	}
+	list, err := net.Listen(networkType, "0.0.0.0:0")
 	if err != nil {
 		return nil, err
 	}
